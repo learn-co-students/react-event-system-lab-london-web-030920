44b4f59522e4119f9523592a3a1dddc1fcee9a14
GeorgeCoops <coopsgeorge@gmail.com>
2020-05-13 10:38:23 +0100

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+const EyesOnMe = () => {
+
+    const focusHandler = () => {
+        console.log("Good!")
+    }
+
+    const blurHandler = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return(
+        <button onFocus={focusHandler} onBlur={blurHandler}></button>
+    )
+
+}
+
+export default EyesOnMe
